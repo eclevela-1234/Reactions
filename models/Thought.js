5196b0f540e6a2b2b79118dc57d8fc24ce16990c
Eliot Cleveland <eclevela@richmond.edu>
2022-09-27 23:38:03 -0400

    added most of thought model, need to add reactions

---
+++ models/Thought.js
@@ -0,0 +1,29 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
--- models/User.js
+++ models/User.js
@@ -1,30 +1,43 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 // const validateEmail = require('../utils/validateEmail')
+const dateFormat = require("../utils/dateFormat");
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     userName: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
     email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: 'Email address is required',
-        // validate: [validateEmail, 'Please fill a valid email address'],
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: "Email address is required",
+      // validate: [validateEmail, 'Please fill a valid email address'],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
     },
     thoughts: [],
-    friends: []
-})
+    friends: [],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 UserSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
-  });
+  return this.friends.length;
+});
 
-  const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
-  module.exports = User;
+module.exports = User;
