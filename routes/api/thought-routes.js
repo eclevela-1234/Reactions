5b4b6d345c143ac6d295c8e0d5d2487c4500023e
Eliot Cleveland <eclevela@richmond.edu>
2022-09-28 20:21:05 -0400

    added controllers and routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,5 +1,16 @@
 const router = require('express').Router();
-
+const {
+    addThought,
+    removeThought,
+    addReaction,
+    removeReaction,
+  } = require("../../controllers/thought-controller");
+  
+  router.route("/:userId").post(addThought);
+  
+  router.route("/:userId/:thoughtId").put(addReaction).delete(removeThought);
+  
+  router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction)
 
 
 
